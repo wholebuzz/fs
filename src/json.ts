8c05fedde23a1d485cc375b4bf29bc6045605168
jfoutts-avenue8 <justin.foutts@avenue8.com>
2021-09-07 21:35:02 -0700

    Add missing deps

--- src/json.ts
+++ src/json.ts
@@ -1,40 +1,11 @@
-import * as crypto from 'crypto'
 import * as readline from 'readline'
 import { Readable } from 'stream'
 import through2 from 'through2'
 import { ReadableStreamTree, WritableStreamTree } from 'tree-stream'
 import { FileSystem } from './fs'
-import { hashStream } from './util'
+import { hashStream, isShardedFilename, shardedFilename, shardIndex } from './util'
 
 const JSONStream = require('JSONStream')
-
-export interface Shard {
-  index: number
-  modulus: number
-}
-
-export const shardIndex = (text: string, modulus: number) =>
-  parseInt(md5(text ?? '').slice(-4), 16) % modulus
-
-export const shardMatchText = (text: string, shard: Shard) =>
-  shardIndex(text, shard.modulus) === shard.index
-
-export const isShardedFilename = (name: string) => name.match(/\-(S+)\-of\-(N+)/)?.[1].length
-
-export const shardedFilename = (name: string, shard: Shard) => {
-  const of = name.lastIndexOf('-of-')
-  const leading = name.lastIndexOf('-', of - 1)
-  const digits = of - leading - 1
-  return digits <= 0
-    ? name
-    : name.substring(0, leading + 1) +
-        `${shard.index}`.padStart(digits, '0') +
-        '-of-' +
-        `${shard.modulus}`.padStart(digits, '0') +
-        name.substring(of + 4 + digits)
-}
-
-export const md5 = (x: string) => crypto.createHash('md5').update(x).digest('hex')
 
 /**
  * Reads every line from a file.
--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,4 @@
+import * as crypto from 'crypto'
 import SHA from 'sha.js'
 import { Readable } from 'stream'
 import through2 from 'through2'
@@ -9,6 +10,34 @@ export const logger = {
   error: (...args: any[]) => console.log(...args),
   info: (...args: any[]) => console.log(...args),
 }
+
+export interface Shard {
+  index: number
+  modulus: number
+}
+
+export const shardIndex = (text: string, modulus: number) =>
+  parseInt(md5(text ?? '').slice(-4), 16) % modulus
+
+export const shardMatchText = (text: string, shard: Shard) =>
+  shardIndex(text, shard.modulus) === shard.index
+
+export const isShardedFilename = (name: string) => name.match(/\-(S+)\-of\-(N+)/)?.[1].length
+
+export const shardedFilename = (name: string, shard: Shard) => {
+  const of = name.lastIndexOf('-of-')
+  const leading = name.lastIndexOf('-', of - 1)
+  const digits = of - leading - 1
+  return digits <= 0
+    ? name
+    : name.substring(0, leading + 1) +
+        `${shard.index}`.padStart(digits, '0') +
+        '-of-' +
+        `${shard.modulus}`.padStart(digits, '0') +
+        name.substring(of + 4 + digits)
+}
+
+export const md5 = (x: string) => crypto.createHash('md5').update(x).digest('hex')
 
 /**
  * Hashes a [[Readable]] stream.
